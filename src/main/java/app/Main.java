143ea71373b72182979e59506f5afa0546d46af5
petert <peter.torgersen@gmail.com>
2024-05-05 11:20:32 +0200

    Foundational setup to get ready for code start

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,19 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
+import app.controllers.TestController;
+import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "carport";
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
     public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
 
@@ -18,5 +27,7 @@ public class Main {
         // Routing
 
         app.get("/", ctx ->  ctx.render("index.html"));
+
+        TestController.addRoute(app, connectionPool);
     }
 }
