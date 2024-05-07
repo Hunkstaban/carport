7e5df049ae3ea6046482a4f8e3e9f580b04fab29
petert <peter.torgersen@gmail.com>
2024-05-07 20:40:49 +0200

    Started on functionality for creating a new order/inquiry, as well as calculating the product list needed.

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,4 +1,39 @@
 package app.controllers;
 
+import app.entities.Product;
+import app.entities.ProductList;
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.OrderMapper;
+import app.persistence.UserMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+
 public class OrderController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.post("newOrder", ctx -> newOrder(ctx, connectionPool));
+    }
+
+    private static void newOrder(Context ctx, ConnectionPool connectionPool) {
+        int carportWidthID = Integer.parseInt(ctx.formParam("carportWidth"));
+        int carportLengthID = Integer.parseInt(ctx.formParam("carportLength"));
+        boolean shed = Boolean.parseBoolean(ctx.formParam("shed"));
+
+        try {
+            int carportWidth = OrderMapper.getWidthByID(carportWidthID, connectionPool);
+            int carportLength = OrderMapper.getLengthByID(carportLengthID, connectionPool);
+            String remark = ctx.formParam("remark");
+            List<Product> productList = OrderMapper.calculateProductList(carportWidth, carportLength, shed, connectionPool);
+            User user = ctx.sessionAttribute("currentUser");
+            // OrderMapper.newOrder(user, productList, remark, connectionPool);
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,4 +1,91 @@
 package app.persistence;
 
+import app.entities.Product;
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class OrderMapper {
+
+
+
+
+    public static List<Product> calculateProductList (int carportWidth, int carportLength, boolean shed, ConnectionPool connectionPool) {
+        List<Product> productList = null;
+        int carportSquareMeters = carportWidth * carportLength;
+        return productList;
+    }
+
+    private static int calculatePosts (int carportLength) {
+        // All carports starts with 4 posts
+        int posts = 4;
+        int remainingLength = (carportLength * 10) - 4300; // 1000mm front unsupported + 3300mm until additional posts needed
+        int additionalPosts;
+        if (remainingLength % 3400 == 0) {
+            additionalPosts = remainingLength / 3400;
+        } else {
+            additionalPosts = remainingLength / 3400 + 1;
+        }
+        posts += (additionalPosts * 2);
+
+        return posts;
+    }
+
+    private static int calculateRoof (int carportSquareMeters) {
+        int roofPlates = 0;
+        return roofPlates;
+    }
+
+    public static int getWidthByID (int carportWidthID, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT * FROM carport_widths WHERE carport_width_id = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, carportWidthID);
+            ResultSet rs = ps.executeQuery();
+
+
+            if (rs.next()) {
+                int carportWidth = rs.getInt("width");
+                return carportWidth;
+
+            } else {
+                throw new DatabaseException("Error no width found");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int getLengthByID (int carportLengthID, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT * FROM carport_lengths WHERE carport_length_id = ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, carportLengthID);
+            ResultSet rs = ps.executeQuery();
+
+
+            if (rs.next()) {
+                int carportLength = rs.getInt("length");
+                return carportLength;
+
+            } else {
+                throw new DatabaseException("Error no length found");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
