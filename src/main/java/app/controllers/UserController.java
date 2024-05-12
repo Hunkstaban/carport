270d93a344b420fed3ff4fdd724eb3d4f7781ed2
vaangis93 <vaangis93@gmail.com>
2024-05-12 12:20:31 +0200

    laver funktionalitet til dropdown. til at hente length and width fra DB og displaye i dropdown create-inquiry.html

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,25 +1,30 @@
 package app.controllers;
 
-import app.entities.User;
+import app.entities.CarportLength;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
-import app.persistence.UserMapper;
+import app.persistence.ProductMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.List;
 
 public class UserController {
 
     public static void addRoute(Javalin app, ConnectionPool connectionPool) {
         app.get("renderLoginPage", ctx -> renderLoginPage(ctx, connectionPool));
-        app.get("/renderSignupPage", ctx -> renderSignupPage(ctx, connectionPool));
+        app.get("/opret-bruger", ctx -> renderSignupPage(ctx, connectionPool));
         app.get("/renderIndex", ctx -> renderIndex(ctx, connectionPool));
         app.get("/acceptInquiry", ctx -> acceptInquiry(ctx, connectionPool));
         app.get("myOrders", ctx -> myOrders(ctx, connectionPool));
         app.get("/createInquiry", ctx -> createInquiry(ctx, connectionPool));
     }
 
-    private static void createInquiry(Context ctx, ConnectionPool connectionPool) {
 
+    private static void createInquiry(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+
+        List<CarportLength> getAllLengthList = ProductMapper.getAllLength(connectionPool);
+        ctx.attribute("getAllLength", getAllLengthList);
         ctx.render("user/create-inquiry.html");
 
     }
